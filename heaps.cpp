ba9d13e1611242a8bd6fe68c7ea442ea712f9bf5
brgccf <brgccf@cin.ufpe.br>
2016-04-13 20:13:04 -0300

    heaps

---
+++ heaps.cpp
@@ -0,0 +1,22 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+vector<int> heap(20);
+
+void heapify()
+{
+
+}
+
+void build_heap()
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+	heap[0] = 0;
+	printf("heap: %d\n", heap[0]);
+	printf("Hello World! :)\n");
+	return 0;
+}
