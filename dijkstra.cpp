bd604607c72e5e0a400c1173c4657e6a8d8a33cf
Bruno Roberto Gouveia C da Cunha Filho <brgccf@ug2c10.windows.cin.ufpe.br>
2016-06-09 15:37:21 -0300

    dijkstra start

---
+++ dijkstra.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+vector< vector<int> > distances; //guardar distancias descobertas aqui
+vector< vector<int> > antecessor; //guardar antecessores aqui
+
+priority_queue< pair<int,int>, vector< pair<int,int> >, greater< pair<int,int> > > myqueue;
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
