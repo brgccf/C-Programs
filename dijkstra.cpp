7781c90ae84f3650018ed5ba69cc65cac71cd5e0
Bruno Roberto Gouveia C da Cunha Filho <brgccf@WINDOWS.CIN.UFPE.BR>
2016-06-10 17:13:02 -0300

    Dijkstra phase 1

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,14 +1,29 @@
 #include "bits/stdc++.h"
 using namespace std;
+#define INF 10001001
+vector< vector<int> > dist; //guardar distancias descobertas aqui
+vector< vector<int> > ant; //guardar antecessores aqui para pegar o caminho final
 
-vector< vector<int> > distances; //guardar distancias descobertas aqui
-vector< vector<int> > antecessor; //guardar antecessores aqui
+priority_queue< pair<int,int>, vector< pair<int,int> >, greater< pair<int,int> > > pq;
 
-priority_queue< pair<int,int>, vector< pair<int,int> >, greater< pair<int,int> > > myqueue;
+void fill(int a, int b)
+{
+	for (int i = a; i < b; ++i)
+	{
+		for(int j = a; j < b)
+		dist[i] = INF;
+		ant[i] = 0;
+	}
+}
 
+void dijkstra(int k)
+{
+	fill(1, k);
+
+}
 
 int main(int argc, char const *argv[])
 {
-	
+		
 	return 0;
 }
