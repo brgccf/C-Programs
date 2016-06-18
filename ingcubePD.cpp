e6f96661474f13de04acebc25f3b6fc0338c7c9a
Bruno Roberto Gouveia C da Cunha Filho <brgccf@ug5c08.windows.cin.ufpe.br>
2016-06-18 11:07:46 -0300

    PD phase 2

--- ingcubePD.cpp
+++ ingcubePD.cpp
@@ -1,14 +1,28 @@
+/*Bruno Filho (brgccf)
+UVa 11137 - Ingenuous Cube
+*/
 #include "bits/stdc++.h"
 using namespace std;
 
-class tab
-{
-public:
-	int key;
-	bool belongs, exist;
-	tab();
+// class tab
+// {
+// public:
+// 	int key;
+// 	bool belongs, exist;
+// 	tab();
 	
-};
+// };
+
+// tab::tab()
+// {
+
+// }
+
+typedef struct tab
+{
+	int key;
+	bool belong, exist;
+}tab;
 
 int main(int argc, char const *argv[])
 {
@@ -24,6 +38,13 @@ int main(int argc, char const *argv[])
 	tab table[21][9262]; //max == 9261
 	table[0][0].key = 0;
 	for(int i = 0; i < 21; ++i) table[0][i].exist = false;
+	for (int i = 1; i < 21; ++i)
+	{
+		for (int j = 0; j < 9262; ++j)
+		{
 
+		}
+	}
+	printf("END\n");
 	return 0;
 }
